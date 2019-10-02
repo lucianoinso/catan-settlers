e29fed9d330e7f3453e6d4c7e4f1bc31bfc81c94
Mauro Cano Brusa <maurocanobrusa@gmail.com>
2019-10-02 15:24:53 +0000

    Esqueleto terminado - serviceWorker eliminado porque no lo vamos a usar. - Ahora anda en firefox. - App se movió a una carpeta separada. - En App hay una propuesta de esqueleto comentada.

---
+++ src/App/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+function App() {
+  return (<div>Esta página está vacía.</div>);
+  /*
+  return (
+    <Header />
+    <Switch>
+      <Route exact path="/" component={Index} />
+      <Route exact path="/login" component={Login} />
+    </Switch>
+    <Footer />
+  );
+  */
+}
+
+export default App;
