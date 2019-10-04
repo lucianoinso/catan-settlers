00c873b47b4e9e92995c485322ef17f2209a7f1d
Lucas Leonardo <lucasleonardo@protonmail.com>
2019-10-04 11:37:25 -0300

    Added needed function.

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import axios from "axios";
 
 class Login extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
+
+  componentDidMount() {}
 
   render() {
     return <div>Pagina Login</div>;
