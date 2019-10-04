0c0ded05a2239dcb939e3a0150b995560356982d
loot <lucianoinso@gmail.com>
2019-10-04 12:10:27 -0300

    Default form added, it needs customization and adding react-redux to the project

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -1,17 +1,63 @@
 import React from "react";
 import axios from "axios";
 
+
 class Login extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
 
-  componentDidMount() {}
+  state = {
+    username: "",
+    password: "",
+  }
+
+  onSubmit = e => {
+    e.preventDefault();
+    console.error("Not implemented!!1");
+  }
+
+  componentDidMount() {
+
+  }
 
   render() {
-    return <div>Pagina Login</div>;
-  }
+    return (
+      <form onSubmit={this.onSubmit}>
+        <fieldset>
+          <legend>Login</legend>
+          <p>
+            <label htmlFor="username">Username</label>
+            <input
+              type="text" id="username"
+              onChange={e => this.setState({username: e.target.value})} />
+          </p>
+          <p>
+            <label htmlFor="password">Password</label>
+            <input
+              type="password" id="password"
+              onChange={e => this.setState({password: e.target.value})} />
+          </p>
+          <p>
+            <button type="submit">Login</button>
+          </p>
+
+          <p>
+            Don't have an account? <Link to="/register">Register</Link>
+          </p>
+        </fieldset>
+      </form>
+  );
 }
 
+const mapStateToProps = state => {
+  return {};
+}
+
+const mapDispatchToProps = dispatch => {
+  return {};
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
 export default Login;
