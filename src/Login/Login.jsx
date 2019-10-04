d5265892ff240466cbdfb34034f1a5673bc8268e
Lucas Leonardo <lucasleonardo@protonmail.com>
2019-10-04 12:08:00 -0300

    Added state and requests.

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -1,13 +1,26 @@
 import React from "react";
 import axios from "axios";
+import MockAdapter from "axios-mock-adapter";
 
 class Login extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+
+    this.state = {
+      user: "",
+      pass: ""
+    };
+
+    this.mock = new MockAdapter(axios);
   }
 
-  componentDidMount() {}
+  componentDidMount() {
+    axios.post("/users/login", this.state);
+
+    this.mock.onPost("/users/login").reply(200, {
+      token: "fgewr234h482o3321j45o3j1"
+    });
+  }
 
   render() {
     return <div>Pagina Login</div>;
