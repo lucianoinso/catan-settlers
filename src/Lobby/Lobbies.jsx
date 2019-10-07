56ce1ef287733e3ca6ea00087879a3aba04981e7
Milagros Gonzalez <mmilagrosgon@gmail.com>
2019-10-07 11:52:08 -0300

    Corrected code of Lobby, now is a class.

--- src/Lobby/Lobbies.jsx
+++ src/Lobby/Lobbies.jsx
@@ -1,52 +1,52 @@
 import React from "react";
 import axios from "axios";
 import axiosMock from "../App/axiosMock.js";
-import Lobby from "./Lobby"
+import Lobby from "./Lobby";
 
 class Lobbies extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      lobbyList : []
-    }
-  };
-
+      lobbyList: []
+    };
+  }
 
   componentDidMount() {
-
-    makeComponentFromLobby ({id, name, owner, players, max_players}){
-  	  return (
-  	  	<li>
-  	  	  <Lobby id={id} name={name} owner={owner} players={players} max_players={max_players} /> 
-  	  	</li>
-      );
-    }
-
     axiosMock.onGet(`/rooms`).reply(200, [
-      {id: 1,
-      name: "Limbo",
-      owner: "beleth",
-      players: ["mila", "karen"],
-      max_players: 3}
-     ]);
+      {
+        id: 1,
+        name: "Limbo",
+        owner: "beleth",
+        players: ["mila", "karen"],
+        max_players: 3
+      }
+    ]);
 
-    axios
-      .get(`/rooms`)
-      .then(response => {
-        this.setState({
-          lobbyList: response.data.map(
-            this.makeComponentFromLobby,
-            this
-          )
-        });
+    axios.get(`/rooms`).then(response => {
+      this.setState({
+        lobbyList: response.data.map(this.makeComponentFromLobby, this)
       });
+    });
     //.catch(error=> { console.log(error);})
-  
+  }
+
+  makeComponentFromLobby({ id, name, owner, players, max_players }) {
+    return (
+      <li>
+        <Lobby
+          id={id}
+          name={name}
+          owner={owner}
+          players={players}
+          max_players={max_players}
+        />
+      </li>
+    );
+  }
 
   render() {
     return <ul className="lobbies">{this.state.lobbyList}</ul>;
   }
- }
 }
 
 export default Lobbies;
--- src/Lobby/Lobby.jsx
+++ src/Lobby/Lobby.jsx
@@ -1,37 +1,31 @@
 import React from "react";
 
 class Lobby extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      id: 0,
-      name: "lobby_name",
-      owner: "owner",
-      players: ["player1", "player2"],
-      max_players: 3
-    }
-  };
-
-function Lobby({id, name, owner, players, max_players}){
-    return(
-    	<ul>
-    	  <li> 
-    	    <div className={`id ${id}`}> lobby id: {id} </div>
-    	  </li>
-    	  <li>
-    	    <div className={`name ${name}`}> name: {name} </div>
-    	  </li>
-    	  <li>
-    	    <div className={`owner ${owner}`}> owner: {owner} </div>
-    	  </li>
-    	  <li>
-    	    <div className={`players ${players}`}> players: {players} </div>
-    	  </li>
-    	  <li>
-    	    <div className={`max_players ${max_players}`}> max_players: {max_players} </div>
-    	  </li>
-        </ul>  
+  render() {
+    const { id, name, owner, players, max_players } = this.props;
+    return (
+      <ul>
+        <li>
+          <div className={`id ${id}`}> lobby id: {id} </div>
+        </li>
+        <li>
+          <div className={`name ${name}`}> name: {name} </div>
+        </li>
+        <li>
+          <div className={`owner ${owner}`}> owner: {owner} </div>
+        </li>
+        <li>
+          <div className={`players ${players}`}> players: {players} </div>
+        </li>
+        <li>
+          <div className={`max_players ${max_players}`}>
+            {" "}
+            max_players: {max_players}{" "}
+          </div>
+        </li>
+      </ul>
     );
+  }
 }
 
 export default Lobby;
