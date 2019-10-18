23547cbb3b985b1699c273aed5472c40c400d726
Mauro Cano Brusa <maurocanobrusa@gmail.com>
2019-10-18 13:42:08 +0000

    Correcciones para que pasen todos los tests. Habíamos usado mal el react-router-dom de modo que un test no pasaba (por más que funcionaba bien). También saque variables que no se usaban para eliminar las warnings.

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import Header from "./Header";
 import Home from "../Home/Home";
@@ -7,18 +7,22 @@ import Login from "../Login/Login";
 import Lobby from "../Lobby/Lobby";
 import Game from "../Game/Game";
 import Footer from "./Footer";
+import PopupController from "../PopupController/PopupController";
 
 function App() {
   return (
     <div>
       <Header />
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/rooms" component={Lobby} />
-        <Route path="/game/:id" component={Game} />
-      </Switch>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/rooms" component={Lobby} />
+          <Route path="/game/:id" component={Game} />
+        </Switch>
+      </BrowserRouter>
       <Footer />
+      <PopupController />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,13 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App/App";
-import { BrowserRouter } from "react-router-dom";
-import PopupController from "./PopupController/PopupController";
 
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-    <PopupController />
-  </BrowserRouter>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
