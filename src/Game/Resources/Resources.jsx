87c4e7ed72ac959f8aa95df564b00c503c2947ea
Lucas Leonardo <lucasleonardo@protonmail.com>
2019-11-04 19:59:50 -0300

    Show popup on each new turn or after trading.

--- src/Game/Resources/Resources.jsx
+++ src/Game/Resources/Resources.jsx
@@ -1,16 +1,91 @@
 import React from "react";
 import { connect } from "react-redux";
-import Resource from "./Resource.jsx";
-import { mapStateToProps, mapDispatchToProps } from "./Resources.ducks.js";
+import PopupController from "../../PopupController/PopupController";
+import Resource from "./Resource";
+import { mapStateToProps, mapDispatchToProps } from "./Resources.ducks";
+import { resourceNames } from "../SatanDictionary";
 
 class Resources extends React.Component {
   constructor(props) {
     super(props);
 
+    this.previousState = {
+      brickAmount: 0,
+      woolAmount: 0,
+      grainAmount: 0,
+      lumberAmount: 0,
+      oreAmount: 0
+    };
+  }
+
+  componentDidMount() {
     this.props.saveResource();
+
+    const {
+      brickAmount,
+      woolAmount,
+      grainAmount,
+      lumberAmount,
+      oreAmount
+    } = this.props;
+
+    this.previousState = {
+      prevBrick: brickAmount,
+      prevWool: woolAmount,
+      prevGrain: grainAmount,
+      prevLumber: lumberAmount,
+      prevOre: oreAmount
+    };
+  }
+
+  updateAndNotify(previous, current, type) {
+    if (previous === current) {
+      return "";
+    }
+
+    const amount = current - previous;
+    this.previousState.current = amount;
+    const changed = `Recibiste ${amount} ${resourceNames[type]}s`;
+    PopupController.pushLog({ content: changed });
+
+    return changed;
+  }
+
+  checkChangedResources() {
+    const {
+      prevBrick,
+      prevWool,
+      prevGrain,
+      prevLumber,
+      prevOre
+    } = this.previousState;
+
+    const {
+      brickAmount,
+      woolAmount,
+      grainAmount,
+      lumberAmount,
+      oreAmount
+    } = this.props;
+
+    let changed = "";
+    let amount = 0;
+
+    changed += this.updateAndNotify(prevBrick, brickAmount, "brick");
+    changed += this.updateAndNotify(prevWool, woolAmount, "wool");
+    changed += this.updateAndNotify(prevGrain, grainAmount, "grain");
+    changed += this.updateAndNotify(prevLumber, lumberAmount, "lumber");
+    changed += this.updateAndNotify(prevOre, oreAmount, "ore");
+
+    if (changed === "") {
+      changed = "Didn't receive any resources";
+      PopupController.pushLog({ content: changed });
+    }
   }
 
   render() {
+    this.checkChangedResources();
+
     return (
       <div className="resourceCards">
         <h4>Cartas de recursos</h4>
