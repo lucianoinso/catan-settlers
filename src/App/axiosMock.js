1900a2e0fca7d5b284ed243062025c11f3c99eaf
Sacul <2219281-Sacules@users.noreply.gitlab.com>
2019-11-16 14:32:44 +0000

    Use env var instead of hardcoding the API URL

--- src/App/axiosMock.js
+++ src/App/axiosMock.js
@@ -1,6 +1,10 @@
 import axios from "axios";
 import MockAdapter from "axios-mock-adapter";
+import apiURL from "../api";
 
-const axiosMock = new MockAdapter(axios, { delayResponse: 150 });
+const axiosMock = new MockAdapter(
+  apiURL === undefined ? axios : axios.create(),
+  { delayResponse: 150 }
+);
 
 export default axiosMock;
