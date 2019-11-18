1470e76f7496ad18565c8737215fb0c9627c68f5
Karen Palacio <karen.palacio.1994@gmail.com>
2019-11-18 16:23:56 +0000

    Primer intento de devcard Knight. El mock está con players vacio para ver si nos sale manejar ese caso. Hay un intento de eso, pero no anda todavia

--- src/Game/Robber/ChooseHex.jsx
+++ src/Game/Robber/ChooseHex.jsx
@@ -3,12 +3,21 @@ import { connect } from "react-redux";
 import { mapStateToProps, mapDispatchToProps } from "./Robber.ducks";
 import ChoosableHex from "./ChoosableHex";
 
+export function checkRobberKnight(availableRobber, availableKnight) {
+  return availableRobber === false ? availableKnight : availableRobber;
+}
+
 class ChooseHex extends React.Component {
   render() {
+    // console.log(this.props.availableRobber);
     if (!this.props.isMovingRobber) return <span />;
+    const availableThing = checkRobberKnight(
+      this.props.availableRobber,
+      this.props.availableKnight
+    );
     return (
       <div>
-        {this.props.availableHexes.map(Hex => (
+        {availableThing.map(Hex => (
           <ChoosableHex
             key={`(${Hex.position.level},${Hex.position.index})`}
             level={Hex.position.level}
